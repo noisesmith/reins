b66780e04acc0d30418733004fe26c0c87b2993b
Justin Smith <noisesmith@gmail.com>
2013-11-27 16:44:49 -0800

    version bump for deploy

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject noisesmith/reins "0.0.1"
+(defproject noisesmith/reins "0.0.2"
   :description "An extension to coordinate many caribou so as to pull your sled to glory."
   :dependencies [[avout "0.5.4"]]
   :license {:name "Eclipse Public License - v 1.0"
